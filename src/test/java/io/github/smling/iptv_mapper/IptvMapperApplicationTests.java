878e05482127d0696c8bf529b1a4469b56fe2a5e
ch-li <smling9_9@hotmail.com>
2025-11-05 22:41:21 +0000

    feat-2: migrate db to test container

--- src/test/java/io/github/smling/iptv_mapper/IptvMapperApplicationTests.java
+++ src/test/java/io/github/smling/iptv_mapper/IptvMapperApplicationTests.java
@@ -2,12 +2,29 @@ package io.github.smling.iptv_mapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-class IptvMapperApplicationTests {
+@Testcontainers
+class IptvMapperMainApplicationTests {
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("iptv-mapper-test")
+            .withUsername("testuser")
+            .withPassword("testpass");
 
-	@Test
-	void contextLoads() {
-	}
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
+    }
 
+    @Test
+    void contextLoads() { }
 }
