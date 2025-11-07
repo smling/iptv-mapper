255818ca4040854cc4d62a4220501501f4415855
ch-li <smling9_9@hotmail.com>
2025-11-07 10:25:46 +0000

    BUG-1: fix missing scheuled

--- src/main/java/io/github/smling/iptv_mapper/IptvMapperApplication.java
+++ src/main/java/io/github/smling/iptv_mapper/IptvMapperApplication.java
@@ -2,10 +2,12 @@ package io.github.smling.iptv_mapper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IptvMapperApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(IptvMapperApplication.class, args);
+        SpringApplication.run(IptvMapperApplication.class, args);
 	}
 }
