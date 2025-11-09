54024f15170f0dab1c77c543c3a1cf02dec6e76e
ch-li <smling9_9@hotmail.com>
2025-11-09 22:30:24 +0000

    FEAT-5/dao: add audit entities createdAt and updatedAt

---
+++ src/main/java/io/github/smling/iptv_mapper/models/dao/AuditEntity.java
@@ -0,0 +1,37 @@
+package io.github.smling.iptv_mapper.models.dao;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.OffsetDateTime;
+
+@MappedSuperclass
+public abstract class AuditEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public OffsetDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+
